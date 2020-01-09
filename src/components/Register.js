35b6585025bc173e6117e3452df984b7bb3a8527
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2020-01-09 17:19:47 -0600

    Fixed form styling

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import {withRouter} from "react-router";
 import {Link} from 'react-router-dom';
-import {Form, Button} from "semantic-ui-react";
+import {Form} from "semantic-ui-react";
 
 import './Forms.scss';
 
@@ -18,7 +18,6 @@ const RegisterForm = props => {
         event.preventDefault();
         axios.post("https://funtimes-dreamteam.herokuapp.com/api/registration/", user)
             .then(res => {
-                // console.log(user)
                 localStorage.setItem('key', res.data.key)
                 props.history.push("/game") //this should be the link to the game
             })
@@ -34,59 +33,47 @@ const RegisterForm = props => {
 
     return(
         <div className="container">
-        <div className="userForm">
-            <h1>Sign Up</h1>
-            <div className="enterform">
-        <Form onSubmit={event => handleSubmit(event)}>
-            <Form.Group>
-                <Form.Input
-                    label="Username"
-                    name="username"
-                    type="text"
-                    value={user.username}
-                    onChange={event => handleChange(event)}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Input
-                    label="Email"
-                    name="email"
-                    type="email"
-                    value={user.email}
-                    onChange={event => handleChange(event)}
-                    
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Input
-                    label="Password"
-                    name="password1"
-                    type="password"
-                    value={user.password1}
-                    onChange={event => handleChange(event)}
-                    
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Input
-                    label="Confirm Password"
-                    name="password2"
-                    type="password"
-                    value={user.password2}
-                    onChange={event => handleChange(event)}
-                    
-                />
-            </Form.Group>
-            <Button
-                type="submit"
-                // postive
-                labelPosition="right"
-                content="Sign Up"
-            />
-            <Link to="/">already have an account?</Link>
-        </Form>
-        </div>
-        </div>
+            <div className="userForm">
+                <h1>Sign Up</h1>
+                    <Form className="enterform" onSubmit={event => handleSubmit(event)}>
+                        <Form.Group>
+                            <Form.Input
+                                label="Username"
+                                name="username"
+                                type="text"
+                                value={user.username}
+                                onChange={event => handleChange(event)}
+                            />
+                            <Form.Input
+                                label="Email"
+                                name="email"
+                                type="email"
+                                value={user.email}
+                                onChange={event => handleChange(event)}
+                                
+                            />
+                            <Form.Input
+                                label="Password"
+                                name="password1"
+                                type="password"
+                                value={user.password1}
+                                onChange={event => handleChange(event)}
+                                
+                            />
+                            <Form.Input
+                                label="Confirm Password"
+                                name="password2"
+                                type="password"
+                                value={user.password2}
+                                onChange={event => handleChange(event)}
+                                
+                            />
+                            <button>Sign Up</button>
+                        </Form.Group>
+                        <p>Already have an account? <Link  to="/">Login</Link></p>
+                        
+                    </Form>
+            </div>
         </div>
     )
 }
